7034d8ac9b0d304b3d12e8532922640e063fa949
Mohamed Shadab <mohamedshadab78@gmail.com>
2020-08-18 20:15:36 +0400

    add parcel and change folder structure

--- legacy/resume.js.legacy
+++
@@ -1,4 +0,0 @@
-if (window.innerWidth <= 1000) {
-  var elem = document.getElementById("resume");
-  elem.classList.remove("z-depth-5");
-}
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 //Tooltip for icons
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   var elems = document.querySelectorAll(".tooltipped");
   var instances = M.Tooltip.init(elems);
 });
