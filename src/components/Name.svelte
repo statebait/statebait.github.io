f4044c24dd925ec91bce5cc5057dcc8a87f2f1b3
Mohamed Shadab <mohamedshadab78@gmail.com>
2020-09-04 12:50:12 +0400

    changed sub text and updated deps

--- src/components/Name.svelte
+++ src/components/Name.svelte
@@ -1,21 +1,19 @@
 <script>
-  import FaHeart from "svelte-icons/fa/FaHeart.svelte";
+  import FaRocket from "svelte-icons/fa/FaRocket.svelte";
 </script>
 
 <div class="text-center">
   <h1
     class="text-3xl sm:text-4xl montserrat font-effect-anaglyph text-bold
-    text-primary mt-5">
+      text-primary mt-5">
     Mohamed Shadab
   </h1>
   <div
-    class="text-lg sm:text-xl text-primary flex space-x-3 items-center
-    justify-center mt-5 font-bold">
+    class="text-2xl text-primary flex space-x-3 items-center font-semibold
+      justify-center mt-5">
     <div class="w-5 h-5">
-      <FaHeart />
+      <FaRocket />
     </div>
-    <div>Dev</div>
-    <div>•</div>
-    <div>Open Source</div>
+    <div>Software Engineer</div>
   </div>
 </div>
