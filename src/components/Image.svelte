983eb8cb88571f099bc9ff5720678766dec89876
Mohamed Shadab <mohamedshadab78@gmail.com>
2020-09-04 21:01:40 +0400

    lighthouse assertion fixes, smaller and webp image

--- src/components/Image.svelte
+++ src/components/Image.svelte
@@ -1,6 +1,9 @@
-<img
-  width="200"
-  height="200"
-  src="/assets/mainpic.png"
-  class="rounded-full shadow-2xl"
-  alt="mohamed shadab" />
+<picture>
+  <source type="image/webp" srcset="/assets/mainpic.webp" />
+  <img
+    class="rounded-full shadow-2xl"
+    width="200"
+    height="200"
+    src="/assets/mainpic.png"
+    alt="mohamed shadab" />
+</picture>
