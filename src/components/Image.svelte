53394081046dfd6b23a0f1651370574978ce6111
Mohamed Shadab <mohamedshadab78@gmail.com>
2020-10-12 20:10:05 +0400

    minor style changes

--- src/components/Image.svelte
+++ src/components/Image.svelte
@@ -1,7 +1,7 @@
 <picture>
   <source type="image/webp" srcset="/assets/mainpic.webp" />
   <img
-    class="rounded-full shadow-2xl animate__animated animate__fadeInDown"
+    class="rounded-full animate__animated animate__fadeInDown custom-shadow"
     width="200"
     height="200"
     src="/assets/mainpic.png"
--- src/components/ProjectCard.svelte
+++ src/components/ProjectCard.svelte
@@ -9,20 +9,18 @@
   .card {
     max-width: 500px;
     background-color: #1f1f1f;
-    -webkit-box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.5);
-    -moz-box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.5);
-    box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.5);
   }
 
   .card:hover {
     -webkit-box-shadow: 0px 5px 10px 0px rgba(0, 0, 0, 0.5);
     -moz-box-shadow: 0px 5px 10px 0px rgba(0, 0, 0, 0.5);
     box-shadow: 0px 5px 10px 0px rgba(0, 0, 0, 0.5);
+    transition: 0.75s;
   }
 </style>
 
 <a
-  class="card text-white flex rounded-lg space-x-4 p-5 mx-3"
+  class="card custom-shadow text-white flex rounded-lg space-x-4 p-5 mx-3"
   target="_blank"
   rel="noopener"
   aria-label={airaLabel}
